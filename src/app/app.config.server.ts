fbc6942659c9e981217ecc039ddda8c081b51e6a
zidiabderrahmen <rhoumazaza@gmail.com>
2026-07-24 23:49:01 +0100

    Refactor server configuration to disable SSR and streamline routing setup

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,11 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
-import { provideRouter } from '@angular/router';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),
-    provideRouter(serverRoutes)
+    provideServerRendering(withRoutes(serverRoutes))
   ]
 };
 
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,10 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'detail/:date',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
