5a6c62aa478fe4b9b7fff06b4229bfb91a72df05
zidiabderrahmen <rhoumazaza@gmail.com>
2026-07-25 00:22:42 +0100

    added nasaApiKey to the envFile too

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -4,7 +4,8 @@ const apiKey = process.env.NASA_API_KEY || 'YOUR_API_KEY_HERE';
 
 const envFile = `export const environment = {
   production: true,
-  nasaApiKey: '${apiKey}'
+  nasaApiKey: '${apiKey}',
+  nasaApiUrl: 'https://api.nasa.gov/planetary/apod'
 };`;
 
 fs.writeFileSync('./src/environments/environment.ts', envFile);
